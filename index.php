a112eeb0a6e6b6de2de89572097fe2f38d2d3bc3
frhtsnc <frhtsnc@gmail.com>
2019-06-08 13:17:54 +0300

    changed index test4

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo "This is a simple web application to auto deployment from github test";
+echo "This is a simple web application to auto deployment from github test3";
 echo "<br>";
 echo "This is a simple web application";
 echo "<br>The correct response to the survey is 5 stars";
